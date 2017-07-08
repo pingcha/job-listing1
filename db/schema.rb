5730041d64d4482880ca05ec784478efba64507c
chenxiaoqing <pincha120@gmail.com>
2017-07-08 20:21:07 +0800

    implement wage part

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170708101216) do
+ActiveRecord::Schema.define(version: 20170708103655) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.string   "contant_email"
+    t.integer  "wage_upper_bound"
+    t.integer  "wage_lower_bound"
+    t.string   "contact_email"
   end
 
   create_table "users", force: :cascade do |t|
